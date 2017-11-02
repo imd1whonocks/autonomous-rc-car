ea61f71d8f44ccaadcccc85b4e24dfbb838e37c1
imd1whonocks <shashank2410@gmail.com>
2017-11-02 20:50:24 +0530

    machine learning algorithm

--- mlp_training.py
+++ mlp_training.py
@@ -1,5 +1,3 @@
-__author__ = 'zhengwang'
-
 import cv2
 import numpy as np
 import glob
@@ -7,7 +5,6 @@ import sys
 from sklearn.model_selection import train_test_split
 
 print('Loading training data...')
-e0 = cv2.getTickCount()
 
 # load training data
 image_array = np.zeros((1, 38400))
@@ -26,60 +23,47 @@ for single_npz in training_data:
     image_array = np.vstack((image_array, train_temp))
     label_array = np.vstack((label_array, train_labels_temp))
 
-print ('Image array shape: ', image_array.shape)
-print( 'Label array shape: ', label_array.shape)
-
+#print ('Image array shape: ', image_array.shape)
+#print( 'Label array shape: ', label_array.shape)
 X = image_array[1:, :]
 y = label_array[1:, :]
 print ('Image array shape: ', X.shape)
 print( 'Label array shape: ', y.shape)
-'''
-e00 = cv2.getTickCount()
-time0 = (e00 - e0)/ cv2.getTickFrequency()
-print 'Loading image duration:', time0
+
 
 # train test split, 7:3
-train, test, train_labels, test_labels = train_test_split(X, y, test_size=0.3)
-
-# set start time
-e1 = cv2.getTickCount()
+split_size = int(X.shape[0]*0.7)
+train_image, test_image = X[:split_size], X[split_size:]
+train_label, test_label = y[:split_size], y[split_size:]
 
 # create MLP
 layer_sizes = np.int32([38400, 32, 4])
-model = cv2.ANN_MLP()
-model.create(layer_sizes)
+model = cv2.ml.ANN_MLP_create()
+model.setLayerSizes(layer_sizes)
 criteria = (cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 500, 0.0001)
-#criteria2 = (cv2.TERM_CRITERIA_COUNT, 100, 0.001)
-params = dict(term_crit = criteria,
-               train_method = cv2.ANN_MLP_TRAIN_PARAMS_BACKPROP,
-               bp_dw_scale = 0.001,
-               bp_moment_scale = 0.0 )
-
-print 'Training MLP ...'
-num_iter = model.train(train, train_labels, None, params = params)
-
-# set end time
+model.setTermCriteria(criteria)
+model.setTrainMethod (0,0.001,0)
+model.setActivationFunction(1)
+print('Training MLP ...')
+e1 = cv2.getTickCount()
+model.train(train_image,cv2.ml.ROW_SAMPLE,train_label)
 e2 = cv2.getTickCount()
 time = (e2 - e1)/cv2.getTickFrequency()
-print 'Training duration:', time
-#print 'Ran for %d iterations' % num_iter
+print('Training duration:', time)
 
 # train data
-ret_0, resp_0 = model.predict(train)
+ret_0, resp_0 = model.predict(train_image)
 prediction_0 = resp_0.argmax(-1)
-true_labels_0 = train_labels.argmax(-1)
-
+true_labels_0 = train_label.argmax(-1)
 train_rate = np.mean(prediction_0 == true_labels_0)
-print 'Train accuracy: ', "{0:.2f}%".format(train_rate * 100)
+print('Train accuracy: ', "{0:.2f}%".format(train_rate * 100))
 
 # test data
-ret_1, resp_1 = model.predict(test)
+ret_1, resp_1 = model.predict(test_image)
 prediction_1 = resp_1.argmax(-1)
-true_labels_1 = test_labels.argmax(-1)
-
+true_labels_1 = test_label.argmax(-1)
 test_rate = np.mean(prediction_1 == true_labels_1)
-print 'Test accuracy: ', "{0:.2f}%".format(test_rate * 100)
+print('Test accuracy: ', "{0:.2f}%".format(test_rate * 100))
 
 # save model
 model.save('mlp_xml/mlp.xml')
-'''
